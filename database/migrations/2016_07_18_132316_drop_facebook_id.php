eb06539b645d6d390aece43394579f0ab0282297
oscar <oscar@localhost.localdomain>
2016-07-18 20:51:34 +0200

    Registro de usuarios por redes sociales

---
+++ database/migrations/2016_07_18_132316_drop_facebook_id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropFacebookId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn(array('facebook_id'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
